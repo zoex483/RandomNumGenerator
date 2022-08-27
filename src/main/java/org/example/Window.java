6327048429c23ade72c9592faf348ab9388e6205
zoex483 <dina.rotenberg483@gmail.com>
2022-08-27 12:54:36 +0300

    Initial commit

---
+++ src/main/java/org/example/Window.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+/**
+ * @zoex483
+ *
+ */
+public class Window extends JFrame {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
